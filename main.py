b47ae4277c2b90a065a723860dcaa88dd335992c
Pedro Sanchez <pedro.sanchez@fatelog.com.br>
2025-05-23 13:53:05 -0300

    Já consegue identificar quando tem uma licença com menos de 30 dias para o vencimento

--- main.py
+++ main.py
@@ -20,27 +20,39 @@ today_complete = datetime.datetime.today()
 today = today_complete.date()
 
 # Aquisição da lista com as licenças
-lista_licenca = pd.read_excel('licencas.xlsx')
+lista_licenca = pd.read_csv('licencas.csv', sep=';')
 df = pd.DataFrame(lista_licenca)
 
 # Converter a coluna Vencimento_Licenca para datetime
 df['Vencimento_Licenca'] = pd.to_datetime(df['Vencimento_Licenca']).dt.date
 
+# Contador de linhas
+linhas = 0
+
 # Itera sobre cada  item dentro da tabela
 try:
     for vencimento in df['Vencimento_Licenca']:
-        if vencimento > today:
+        dias = vencimento - today
+        nome_licenca = df['Nome_Licenca'].iloc[linhas]
+
+        if dias.days > 30:
+            print()
+            print(nome_licenca)
             print('Ainda Okay')
 
-        elif vencimento < today:
+        elif dias.days < 0:
+            print()
+            print(nome_licenca)
             print('Vencida')
 
-        elif vencimento == today:
+        elif dias.days == 0:
+            print()
+            print(nome_licenca)
             print('Vencimento hoje!')
 
         else:
             raise Exception("ERRO! VALOR NÃO ESPERADO! CONFERIR BASE DE DADOS")
+        
+        linhas += 1
 except Exception as e:
     print(e)
-
-print(df)
